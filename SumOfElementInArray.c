dd43607b4fcfe43ccd0be0365454158700bde394
Induja <induja.pradeep@outlook.com>
2025-09-01 20:26:37 +0400

    Program to find the sum of all the elements in an array

---
+++ SumOfElementInArray.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("Enter the size of the array:");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Enter the %d elements in array:",n);
+	for(i=0;i<n;i++) scanf("%d",&arr[i]);
+	int sum=0;
+	for(i=0;i<n;i++)      	sum=sum+arr[i];  
+	printf("%d is the sum of all the elements",sum);
+}
+
