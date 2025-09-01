e3f2aac42db5029a2dd215e1dc3bfc5d4bc71e11
Induja <induja.pradeep@outlook.com>
2025-09-01 17:46:58 +0400

    Average of 3 Numbers Program

---
+++ Average.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float a,b,c;
+	printf("Enter 3 Numbers:");
+	scanf("%d %d %d",&a,&b,&c);
+	float avg=(a+b+c)/3;
+	printf("Average:%d",avg);
+}
