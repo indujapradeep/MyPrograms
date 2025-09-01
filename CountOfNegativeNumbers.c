3f6bae1f755690979c0ec7755bec35cc3f741ce3
Induja <induja.pradeep@outlook.com>
2025-09-01 20:17:46 +0400

    Program to count the number of negative numbers in an array

---
+++ CountOfNegativeNumbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("Enter the size of the array:");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Enter the %d elements in array:",n);
+	for(i=0;i<n;i++) scanf("%d",&arr[i]);
+	int count=0;
+	for(i=0;i<n;i++)      if(arr[i]<0)   	count++;  
+	printf("There are %d negative numbers",count);
+}
+
