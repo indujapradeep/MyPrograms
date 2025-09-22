6ef9e67399941c91543e442a98c2d4e3d457bc61
Induja <induja.pradeep@outlook.com>
2025-09-22 19:51:13 +0400

    Program to add two integer values

---
+++ AdditonOfTwoNumbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+	int a,b;
+	printf("Enter a value for A:");
+	scanf("%d",&a);
+	printf("Enter a value for B:");
+	scanf("%d",&b);
+	printf("Performing Addition Operations (A+B)");
+	printf("\n%d + %d = %d",a,b,a+b);
+}
