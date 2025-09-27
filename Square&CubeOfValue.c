36b3211ffc6d5491ea0469b5ac1775494c3eaf01
Induja <induja.pradeep@outlook.com>
2025-09-27 20:45:15 +0400

    Program to find the square and cube of a number

---
+++ Square&CubeOfValue.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b;
+	printf("Enter a value to find the square of a number:");
+	scanf("%d",&a);
+	printf("Square of the %d is %d",a,a*a);
+	printf("\nEnter a value to find the cube of a number:");
+	scanf("%d",&b);
+	printf("Cube of %d is %d",b,b*b*b);
+}
