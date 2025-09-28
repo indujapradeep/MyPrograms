39aa52004681ef09dafc93bbc8e0444c143e45f8
Induja <induja.pradeep@outlook.com>
2025-09-28 21:16:25 +0400

    Program to find the Factorial of a Positive Integer

---
+++ Factorial.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int multiplyNumbers(int n)
+{
+    if (n>=1)
+        return n*multiplyNumbers(n-1);
+    else
+        return 1;
+}
+
+int main() 
+{
+    int n;
+    printf("Enter a positive integer:");
+    scanf("%d",&n);
+    printf("Factorial of %d is %d", n, multiplyNumbers(n));
+}
+
+
+
