8f2c3e61fe0456d59d47912a3a1291bfce65f464
Induja <induja.pradeep@outlook.com>
2025-10-11 21:22:34 +0400

    Program to perform multiplication on 2 numbers

---
+++ Multiplication.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b;
+	printf("Enter the value of A:");
+	scanf("%d",&a);
+	printf("Enter the value of B:");
+	scanf("%d",&b);
+	printf("Performing Multiplication");
+	printf("\n%d x %d = %d",a,b,a*b);
+}
